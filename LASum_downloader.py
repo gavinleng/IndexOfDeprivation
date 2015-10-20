a3d34b84cf1fefe2c133b588da79ba35347adaef
Gavin <gleng.email@gmail.com>
2015-10-20 10:36:22 +0100

    add log and error files

--- LASum_downloader.py
+++ LASum_downloader.py
@@ -14,6 +14,7 @@ import urllib
 import pandas as pd
 import argparse
 import json
+import datetime
 
 
 # url = "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6884/1871689.xls"
@@ -29,18 +30,26 @@ def download(url, sheet, reqFields, outPath):
     try:
         socket = urllib.request.urlopen(url)
     except urllib.error.HTTPError as e:
+        errfile.write(str(now()) + ' excel download HTTPError is ' + str(e.code) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
         sys.exit('excel download HTTPError = ' + str(e.code))
     except urllib.error.URLError as e:
+        errfile.write(str(now()) + ' excel download URLError is ' + str(e.args) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
         sys.exit('excel download URLError = ' + str(e.args))
     except Exception:
         print('excel file download error')
         import traceback
+        errfile.write(str(now()) + ' generic exception: ' + str(traceback.format_exc()) + ' . End progress\n')
+        logfile.write(str(now()) + ' error and end progress\n')
         sys.exit('generic exception: ' + traceback.format_exc())
 
     #operate this excel file
+    logfile.write(str(now()) + ' excel file loading\n')
     xd = pd.ExcelFile(socket)
     df = xd.parse(sheet)
 
+    logfile.write(str(now()) + ' data reading\n')
     print('data reading------')
     raw_data = df.loc[:, col]
 
@@ -54,7 +63,9 @@ def download(url, sheet, reqFields, outPath):
     print('writing to file ' + dName)
     dfw = pd.DataFrame(raw_data_w, columns=col)
     dfw.to_csv(dName, index=False)
+    logfile.write(str(now()) + ' has been extracted and saved as ' + str(dName) + '\n')
     print('Requested data has been extracted and saved as ' + dName)
+    logfile.write(str(now()) + ' finished\n')
     print("finished")
 
 def checkRaw(data, field):
@@ -67,6 +78,9 @@ def checkRaw(data, field):
             print(data.loc[i, :])
 
     return inrow
+
+def now():
+    return datetime.datetime.now()
 
 
 parser = argparse.ArgumentParser(description='Extract online Index of Deprivation Excel file LA Summaries ID 2010 to .csv file.')
@@ -81,15 +95,28 @@ if args.generateConfig:
            "reqFields": ['LA CODE', 'LA NAME', 'Rank of Local Concentration']
            }
 
-    with open("config_LASum.json", "w") as outfile:
+    logfile = open("log_tempLASum.log", "w")
+    logfile.write(str(now()) + ' start\n')
+
+    errfile = open("err_tempLASum.err", "w")
+
+    with open("config_tempLASum.json", "w") as outfile:
         json.dump(obj, outfile, indent=4)
+        logfile.write(str(now()) + ' config file generated and end\n')
         sys.exit("config file generated")
 
 if args.configFile == None:
-    args.configFile = "config_LASum.json"
+    args.configFile = "config_tempLASum.json"
 
 with open(args.configFile) as json_file:
     oConfig = json.load(json_file)
+
+    logfile = open('log_' + oConfig["outPath"].split('.')[0] + '.log', "w")
+    logfile.write(str(now()) + ' start\n')
+
+    errfile = open('err_' + oConfig["outPath"].split('.')[0] + '.err', "w")
+
+    logfile.write(str(now()) + ' read config file\n')
     print("read config file")
 
 download(oConfig["url"], oConfig["sheet"], oConfig["reqFields"], oConfig["outPath"])
